d680599ac1e8ee94bc4606c50aadb58a25c603b6
rboddy <avidryanb@gmail.com>
2024-08-01 23:59:03 -0400

    The last commit of my third decade

---
+++ cmd/web/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+  mux := http.NewServeMux()
+
+  fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+  mux.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
+
+  mux.HandleFunc("GET /{$}", home)
+  mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+  mux.HandleFunc("GET /snippet/view/create", snippetCreate)
+  mux.HandleFunc("POST /snippet/view/create", snippetCreatePost)
+
+  log.Print("starting server on :4000")
+
+  err := http.ListenAndServe(":4000", mux)
+  log.Fatal(err)
+}
